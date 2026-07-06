73dcc019238c951498b872e49376606e78b0c579
menu <lahkuhasunem@gmail.com>
2026-07-06 08:31:12 +0545

    corrected urls

--- blog_platform/urls.py
+++ blog_platform/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, TagViewSet, PostViewSet, CommentViewSet, RegisterView
+from .views import CategoryViewSet, TagViewSet, PostViewSet, CommentViewSet, RegisterView, LoginView
 from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
@@ -13,6 +13,6 @@ router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', obtain_auth_token, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('', include(router.urls)),
 ]
